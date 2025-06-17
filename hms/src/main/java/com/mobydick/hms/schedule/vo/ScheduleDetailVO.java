a85e037c5e979eba281b5ea88a05dfb62e2629e3
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-06-17 16:41:25 +0900

    [add] 스케줄 상세 vo 커밋 누락 2025/06/17

---
+++ hms/src/main/java/com/mobydick/hms/schedule/vo/ScheduleDetailVO.java
@@ -0,0 +1,21 @@
+package com.mobydick.hms.schedule.vo;
+
+import lombok.Data;
+
+import java.sql.Date;
+
+// 스케줄 정보를 담는 VO
+@Data
+public class ScheduleDetailVO {
+
+    private String scheId;
+    private String scheDate;
+    private String scheShiftName;
+    private String emplName;
+    private String workdName;
+    private String workdContext;
+    private String workdImpoName;
+    private String workdStateName;
+    private String roomName;
+
+}
